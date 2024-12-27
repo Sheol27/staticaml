4b49ab8a617f8fbb5154203b3f07f489fe82764b
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-12-27 23:03:01 +0100

    Use float instead of int

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 open Staticaml.Statistics
 
-let possible_values = [1; 2; 3; 4; 5; 6]
+let possible_values = [1.0; 2.0; 3.0; 4.0; 5.0; 6.0]
 
-let probability = cumulative_distribution_function 2 possible_values
+let probability = cumulative_distribution_function 2.0 possible_values
 
 let () = Staticaml.Utils.print_probability probability

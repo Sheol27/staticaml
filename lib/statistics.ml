69091661f53eed7e514d029fe8689cb9f3c113ac
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-12-27 20:56:59 +0100

    Change formatting parameters

--- lib/statistics.ml
+++ lib/statistics.ml
@@ -1,20 +1,16 @@
-let evaluate_probability values conditions =
-  let verify value =
-    List.for_all (fun condition -> condition value) conditions
-  in
+let evaluate_probability (values : int list) (conditions : (int -> bool) list) : float =
+  let verify value = List.for_all (fun condition -> condition value) conditions in
   let results = List.map verify values in
   float_of_int (Utils.count_true results) /. float_of_int (List.length values)
 
 let cumulative_distribution_function target values =
-  let[@tail_mod_cons] rec cumulative_distribution_function' target values
-      accumulator =
+  let[@tail_mod_cons] rec cumulative_distribution_function' target values accumulator =
     match target with
     | 0 ->
         accumulator
     | _ ->
         let is_equal_to_x = Utils.is_equal_to target in
         let probability = evaluate_probability values [is_equal_to_x] in
-        cumulative_distribution_function' (target - 1) values
-          (accumulator +. probability)
+        cumulative_distribution_function' (target - 1) values (accumulator +. probability)
   in
   cumulative_distribution_function' target values 0.0
--- lib/utils.ml
+++ lib/utils.ml
@@ -1,9 +1,11 @@
-let count_true bool_list =
-  List.fold_left (fun acc b -> if b then acc + 1 else acc) 0 bool_list
+let count_true bool_list = List.fold_left (fun acc b -> if b then acc + 1 else acc) 0 bool_list
 
-let print_probability number =
-  Printf.printf "\nProbability is: %.2f%%" (number *. 100.0)
+let print_probability number = Printf.printf "\nProbability is: %.2f%%" (number *. 100.0)
 
 let is_equal_to value target = value = target
 
 let is_even number = number mod 2 = 0
+
+let derivative fx =
+  let dx = 0.001 in
+  fun x -> (fx (x +. dx) -. fx x) /. dx

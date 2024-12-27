f5ef67eea9aba895821e05f6a0a26599bc80b982
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-12-27 20:51:25 +0100

    Change the cumulative_distribution_function to be tail recursive

--- lib/statistics.ml
+++ lib/statistics.ml
@@ -5,11 +5,16 @@ let evaluate_probability values conditions =
   let results = List.map verify values in
   float_of_int (Utils.count_true results) /. float_of_int (List.length values)
 
-let[@tail_mod_cons] rec cumulative_distribution_function target values =
-  match target with
-  | 0 ->
-      0.
-  | _ ->
-      let is_equal_to_x = Utils.is_equal_to target in
-      let probability = evaluate_probability values [is_equal_to_x] in
-      probability +. cumulative_distribution_function (target - 1) values
+let cumulative_distribution_function target values =
+  let[@tail_mod_cons] rec cumulative_distribution_function' target values
+      accumulator =
+    match target with
+    | 0 ->
+        accumulator
+    | _ ->
+        let is_equal_to_x = Utils.is_equal_to target in
+        let probability = evaluate_probability values [is_equal_to_x] in
+        cumulative_distribution_function' (target - 1) values
+          (accumulator +. probability)
+  in
+  cumulative_distribution_function' target values 0.0
